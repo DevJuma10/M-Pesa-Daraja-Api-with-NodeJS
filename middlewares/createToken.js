be6439fed8c27d8cc2a333fae867521fcef8049b
DevJuma10 <juma.study@gmail.com>
2024-04-06 23:44:22 +0300

    Fix: create token function cleanup

--- middlewares/createToken.js
+++ middlewares/createToken.js
@@ -8,25 +8,7 @@ const createToken = async (req, res, next) => {
     const consumer = process.env.CONSUMER_KEY
     const auth = new Buffer.from(`${consumer}:${secret}`).toString("base64");
     const url_dev = "https://sandbox.safaricom.co.ke/oauth/v1/generate?grant_type=client_credentials";
-    // await axios
-    //   .get(url_dev,
-    //     {
-    //       headers: {
-    //         authorization: `Basic ${auth}`,
-    //       },
-    //     }
-    //   )
-    //   .then((data) => {
-    //     console.log(data)
-    //     token = data.data.access_token;
-    //     console.log(data.data);
-    //     next();
-    //   })
-    //   .catch((err) => {
-    //     console.log(err);
-    //     res.status(400).json("TOKEN GENERETION ERROR: " + err.message);
-    //   });
-
+    
     unirest('GET', url_dev)
     .headers({ 'Authorization': `Basic ${auth}` })
     .send()
@@ -34,17 +16,8 @@ const createToken = async (req, res, next) => {
         if (res.error) throw new Error(res.error);
         console.log(res.raw_body);
     });
+    next()
   };
    
 
 export default createToken;
-
-
-
-
-
-// .send()
-// .end(res => {
-//     if (res.error) throw new Error(res.error);
-//     console.log(res.raw_body);
-// });
