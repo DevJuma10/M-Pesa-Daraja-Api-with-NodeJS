00526f1df82818a91776b6bc0eed2f5b4e210b26
DevJuma10 <juma.study@gmail.com>
2024-04-09 11:20:06 +0300

    Feat: Add STK Push query endpoint

--- controllers/stkpushQuery.js
+++ controllers/stkpushQuery.js
@@ -1,23 +1,15 @@
-/**
- * Use this API to check the status of a Lipa Na M-Pesa Online Payment.
- */
 import unirest from "unirest";
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
 dotenv.config();
-// {    
-//     "BusinessShortCode":"174379",    
-//     "Password": "MTc0Mzc5YmZiMjc5TliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMTYwMjE2MTY1NjI3",    
-//     "Timestamp":"20160216165627",    
-//     "CheckoutRequestID": "ws_CO_260520211133524545",    
-//  }   
+import passwordGenerator from "./utils/passwordGenerator.js";
 
 const query_url = 'https://sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query';
 
-const  stkPushQuery = async (req, res) => {
+const stkPushQuery = async (req, res) => {
     
     // Get Token from req header
-    let token = req.token
-    const { CheckoutRequestID } = req.body
+    let token = req.token;
+    const { checkoutRequestID } = req.body;
 
     await unirest('POST', query_url)
             .headers({
@@ -25,30 +17,24 @@ const  stkPushQuery = async (req, res) => {
                 'Authorization' :   `Bearer ${token}`
             })
             .send(JSON.stringify({
-                "BusinessShortCode": procees.env.MPESA_SHORTCODE,
-                "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjQwNDA5MTAyNTM1",
-                "Timestamp": "20240409102535",
-                "CheckoutRequestID": CheckoutRequestID,
+                "BusinessShortCode": process.env.MPESA_SHORTCODE,
+                "Password": passwordGenerator.password,
+                "Timestamp": passwordGenerator.timeStamp,
+                "CheckoutRequestID": checkoutRequestID,
             }))
-}
+            .end(response => {
+                if (response.error) {
+                    console.error(response.error);
+                    console.log(response.body); // Log response body
+                    return res.status(500).json({ error: 'Failed to query M-Pesa' });
+                }
+
+                res.status(200).json({
+                    message: 'success',
+                    data: response.body
+                });
+               
+            });
+};
 
 export default stkPushQuery;
-
-
-
-let unirest = require('unirest');
-let req = unirest('POST', 'https:/sandbox.safaricom.co.ke/mpesa/stkpushquery/v1/query')
-.headers({
-    'Content-Type': 'application/json',
-    'Authorization': 'Bearer ZkyQrNYWUeF2fPaBOnyY7CA3lv6N'
-})
-.send(JSON.stringify({
-    "BusinessShortCode": 174379,
-    "Password": "MTc0Mzc5YmZiMjc5ZjlhYTliZGJjZjE1OGU5N2RkNzFhNDY3Y2QyZTBjODkzMDU5YjEwZjc4ZTZiNzJhZGExZWQyYzkxOTIwMjQwNDA5MTAyNTM1",
-    "Timestamp": "20240409102535",
-    "CheckoutRequestID": "ws_CO_08042024141900404710182419",
-  }))
-.end(res => {
-    if (res.error) throw new Error(res.error);
-    console.log(res.raw_body);
-});
